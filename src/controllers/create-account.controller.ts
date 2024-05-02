0221125647828106bbc845d2e65ead44affeb9b2
Arley Souto <macx15@hotmail.com>
2024-05-02 15:21:20 -0300

    feat: configurado teste com swc

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,9 +1,9 @@
 import { ConflictException, UsePipes } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "@/prisma/prisma.service";
 import { hash } from "bcryptjs";
 import { z } from "zod";
-import { ZodValidationPipe } from "src/pipes/zod-validations-pipe";
+import { ZodValidationPipe } from "@/pipes/zod-validations-pipe";
 
 const createAccountBodySchema = z.object({
   name: z.string(),
