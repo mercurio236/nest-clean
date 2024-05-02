f8440c14515fd237a9aba931a6cb21a668a0cf1a
Arley Souto <macx15@hotmail.com>
2024-05-02 13:47:51 -0300

    fix: ajustado erro de importação do decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, UseGuards } from "@nestjs/common";
 import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -9,6 +10,7 @@ export class CreateQuestionController {
 
   @Post()
   async handle(@CurrentUser() user: UserPayload) {
+    console.log(user);
     return "ok";
   }
 }
